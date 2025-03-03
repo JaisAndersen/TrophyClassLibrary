ae6e4e01ee2096b7da1253a82d3248c2707cdada
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-03 14:35:52 +0100

    Test classe added

---
+++ TrophyClassLibraryTests/TrophyTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrophyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace TrophyClassLibrary.Tests
+{
+    [TestClass()]
+    public class TrophyTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Trophy trophy = new Trophy();
+            trophy.Id = 1;
+            trophy.Competition = "CompetitionName";
+            trophy.Year = 2000;
+            string result = trophy.ToString();
+            Assert.AreEqual("Id: 1, Competition: CompetitionName, Year: 2000", result);
+        }
+    }
+}
