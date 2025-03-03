91f9385426b44a4c9383f2d5e8ecc792444c3a76
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-03 12:27:16 +0100

    Add project files.

---
+++ TrophyClassLibrary/Trophy.cs
@@ -0,0 +1,30 @@
+﻿namespace TrophyClassLibrary
+{
+    public class Trophy
+    {
+        private string competition;
+
+        public int Id { get; set; }
+        public string Competition
+        {
+            get => competition;
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("Competition must not be null!");
+                }
+                if (value.Length < 3)
+                {
+                    throw new ArgumentOutOfRangeException("Competition must be at least 3 charachters long!");
+                }
+                competition = value;
+            }             
+        }
+        public int Year { get; set; }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Competition: {Competition}, Year: {Year}";
+        }
+    }
+}
