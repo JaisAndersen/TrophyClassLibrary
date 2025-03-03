c6e6b5c33bb211561b07eff083f3d09d43eec855
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-03 19:12:03 +0100

    TrophyTest

--- TrophyClassLibraryTests/TrophyTests.cs
+++ TrophyClassLibraryTests/TrophyTests.cs
@@ -13,6 +13,24 @@ namespace TrophyClassLibrary.Tests
     public class TrophyTests
     {
         [TestMethod()]
+        public void TrophyTest()
+        {
+            Trophy trophy = new Trophy();
+            trophy.Id = 1;
+            trophy.Competition = "Cykling";
+            trophy.Year = 1990;
+
+            Assert.AreEqual(1, trophy.Id);
+            Assert.AreEqual("Cykling", trophy.Competition);
+            Assert.AreEqual(1990, trophy.Year);
+
+            Assert.ThrowsException<ArgumentNullException>(() => trophy.Competition = null);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Competition = "AB");
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Year = 1969);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => trophy.Year = 2025);
+        }
+
+        [TestMethod()]
         public void ToStringTest()
         {
             Trophy trophy = new Trophy();
